a72b28778832db0d0b98b80d73cb26edb9327e63
Giorgio Grassini <giorgiograssini@gmail.com>
2021-09-19 13:02:06 +0200

    newsletter

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -1,17 +1,44 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-import Layout from '../layout';
+import Layout from "../layout";
 import * as Components from "../components";
+import { useStaticQuery, graphql } from "gatsby";
+import Card from '../templates/Card';
 
-const Newsletter = ({ location }) => (
-  <Layout location={location} >
-    <Components.SEO title="Newsletter" />
-  </Layout>
-);
+const Newsletter = ({ location }) => {
+  const {
+    allMarkdownRemark: { edges },
+  } = useStaticQuery(
+    graphql`
+      query {
+        allMarkdownRemark(sort: { order: DESC, fields: frontmatter___date }) {
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+                description
+                img
+              }
+              fields {
+                slug
+              }
+            }
+          }
+        }
+      }
+    `
+  );
+  return (
+    <Layout location={location}>
+      <Components.SEO title="Newsletter" />
+      <div class="container">
+        {edges.map((post) => (
+          <Card {...post} key={post.node.id} />
+        ))}
+      </div>
+    </Layout>
+  );
+};
 
 export default Newsletter;
-
-Newsletter.propTypes = {
-  location: PropTypes.shape(),
-};
