b6b65504cbee1e825de9969be40482c14fe7432f
Giorgio Grassini <giorgiograssini@gmail.com>
2022-03-24 12:36:09 +0100

    refactor footer

--- src/components/Footer/Footer.component.jsx
+++ src/components/Footer/Footer.component.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 
+const Links = [
+  {
+    name: "Github",
+    link: "https://github.com/girgetto",
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/girgettoIT",
+  },
+  {
+    name: "dev.to",
+    link: "https://dev.to/girgetto",
+  },
+  {
+    name: "Instagram",
+    link: "https://instagram.com/girgetto.it",
+  },
+];
+
+const LinkComponent = ({ link, name }) => (
+  <a href={link} className="link">
+    {name}
+  </a>
+);
+
 const Footer = () => (
   <footer className="footer">
     <div className="social-links">
-      <a href="https://github.com/girgetto" className="link">
-        Github
-      </a>
-      <a href="https://twitter.com/girgettoIT" className="link">
-        Twitter
-      </a>
-      <a href="https://dev.to/girgetto" className="link">
-        dev.to
-      </a>
-      <a href="https://instagram.com/girgetto.it" className="link">
-        Instagram
-      </a>
+      {Links.map((link, index) => (
+        <LinkComponent key={index} {...link} />
+      ))}
     </div>
     <p>Powered by Gatsby & Github</p>
   </footer>
