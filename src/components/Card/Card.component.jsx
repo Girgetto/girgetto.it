9b945066add6ffe91899b4deefd27f884d4abc00
Giorgio Grassini <giorgiograssini@gmail.com>
2023-04-17 17:36:38 +0200

    style: better link hover

--- src/components/Card/Card.component.jsx
+++ src/components/Card/Card.component.jsx
@@ -3,18 +3,17 @@ import { Link } from "gatsby";
 
 const Card = ({ node }) => {
   return (
-    <Link to={node.fields.slug}>
-      <div className="card">
+    <div className="card">
+      <Link to={node.fields.slug}>
         <header className="description">
           <div className="section">
             <h2>{node.frontmatter.title}</h2>
             <p>{node.frontmatter.description}</p>
           </div>
         </header>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 };
 
 export default Card;
-
